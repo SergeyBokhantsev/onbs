f084aa7e0328fd9c4e8aa790fc15469546fb4d6c
SergeyBokhantsev <sergey.bokhantsev@invisiblecrm.com>
2015-06-06 21:42:51 +0300

    uncommited files

---
+++ Interfaces/IAutomationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    /// <summary>
+    /// http://wiki.linuxquestions.org/wiki/List_of_keysyms
+    /// </summary>
+    public enum AutomationKeys
+    {
+        plus = 0x002b,
+        comma = 0x002c,
+        minus = 0x002d,
+        Down = 0x0600,
+        Left = 0x0601,
+        Right = 0x0602,
+        Up = 0x0603,
+        Control = 0x0702,
+    };
+
+    public interface IAutomationController : IController
+    {
+        void Key(params AutomationKeys[] key);
+    }
+}
---
+++ UIController/Models/NavigationAppPage.cs
@@ -0,0 +1,59 @@
+﻿using Interfaces;
+using Interfaces.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIController.Models
+{
+    public class NavigationAppPage : ExternalApplicationPage
+    {
+        private readonly IAutomationController automation;
+
+        public NavigationAppPage(IHostController hostController, string navAppKey)
+            :base(typeof(ExternalApplicationPage).Name, 
+            hostController.ProcessRunnerFactory.Create(navAppKey),
+            hostController.Dispatcher,
+            hostController.Logger,
+            hostController.GetController<IUIController>())
+        {
+            automation = hostController.GetController<IAutomationController>();
+        }
+
+        protected override void DoAction(PageModelActionEventArgs args)
+        {
+            switch (args.ActionName)
+            {
+                case "F1":
+                    automation.Key(AutomationKeys.Control, AutomationKeys.minus);
+                    break;
+
+                case "F2":
+                    automation.Key(AutomationKeys.Up);
+                    break;
+
+                case "F3":
+                    automation.Key(AutomationKeys.Control, AutomationKeys.plus);
+                    break;
+
+                case "F5":
+                    automation.Key(AutomationKeys.Left);
+                    break;
+
+                case "F6":
+                    automation.Key(AutomationKeys.Down);
+                    break;
+
+                case "F7":
+                    automation.Key(AutomationKeys.Right);
+                    break;
+
+                default:
+                    base.DoAction(args);
+                    break;
+            }
+        }  
+    }
+}
