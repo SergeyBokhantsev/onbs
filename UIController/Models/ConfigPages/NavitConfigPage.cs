96e0c6fe85ab2108018b9a336b063add05cdb901
sergey bokhantsev <sergey.bokhantsev@invisiblecrm.com>
2015-06-22 22:18:37 +0300

    navit config page

---
+++ UIController/Models/ConfigPages/NavitConfigPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces;
+
+namespace UIController.Models.ConfigPages
+{
+    public class NavitConfigPage : ModelBase
+    {
+        private readonly IHostController hc;
+
+        public NavitConfigPage(IHostController hc)
+            :base("SystemConfigurationPage", hc.Dispatcher, hc.Logger)
+        {
+            this.hc = hc;
+
+
+        }
+
+        protected override void DoAction(Interfaces.UI.PageModelActionEventArgs actionArgs)
+        {
+            
+        }
+    }
+}
