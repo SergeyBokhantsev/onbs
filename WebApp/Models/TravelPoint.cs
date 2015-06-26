405ca8b88e5c989f8acb05f061e4b1268bcffa7c
SergeyBokhantsev <sergey.bokhantsev@invisiblecrm.com>
2015-06-26 21:10:31 +0300

    extend model

--- WebApp/Models/TravelPoint.cs
+++ WebApp/Models/TravelPoint.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,14 @@ namespace WebApp.Models
     public class TravelPoint
     {
         public int ID { get; set; }
+        [Required]
         public double Lat { get; set; }
+        [Required]
         public double Lon { get; set; }
+        [Required]
+        public DateTime Time { get; set; }
+        public double Speed { get; set; }
+        [MaxLength(50)]
+        public string Description { get; set; }
     }
 }
