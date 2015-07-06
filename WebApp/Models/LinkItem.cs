9ba0a7be5910c3efb9d863f3b95a4e370ae4c347
sergey bokhantsev <sergey.bokhantsev@invisiblecrm.com>
2015-07-06 21:40:21 +0300

    +files

---
+++ WebApp/Models/LinkItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class LinkItem
+    {
+        public string Caption { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public object Args { get; set; }
+    }
+}
---
+++ WebApp/Models/TravelsList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class TravelsList
+    {
+        public IEnumerable<LinkItem> Today { get; set; }
+        public IEnumerable<LinkItem> Yesterday { get; set; }
+        public TravelsList()
+        {
+        }
+    }
+}
