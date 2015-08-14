e2f3fece63ea3e5083d322067d6b9d611f1ea1c8
Sergey Bokhantsev <sergey.bokhantsev@invisiblecrm.com>
2015-08-14 21:13:18 +0300

    missed file

---
+++ Elm327/Elm327Response.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elm327
+{
+    public interface IElm327Response
+    {
+        Elm327FunctionTypes Type { get; }
+    }
+
+    public class Elm327Response<T> : IElm327Response
+    {
+        public T Value { get; private set; }
+
+        public Elm327FunctionTypes Type { get; private set; }
+
+        public Elm327Response(Elm327FunctionTypes type, T value)
+        {
+            Type = type;
+            Value = value;
+        }
+    }
+}
