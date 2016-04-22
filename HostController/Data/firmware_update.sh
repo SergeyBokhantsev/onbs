ed680460cc845d0c1ecb87d96971ffa738993b14
SergeyBokhantsev <sergey.bokhantsev@invisiblecrm.com>
2016-04-22 21:51:16 +0200

    fixes on rpi

--- HostController/Data/firmware_update.sh
+++ HostController/Data/firmware_update.sh
@@ -2,6 +2,8 @@ cd /home/pi/onbs/
 sudo git stash
 sudo git pull
 
+sleep 15
+
 sudo rm -rf /home/pi/onbs/_bin_back
 sudo mv /home/pi/onbs/_bin /home/pi/onbs/_bin_back
 sudo xbuild /p:Configuration=Release OnboardSytem.sln
